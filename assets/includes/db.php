bf4e398a733ac561a6c5c1c7c06a8f742d410ad4
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-10-02 16:58:59 +0200

    Renomme $pdo en $bdd dans db.php pour corriger une erreur de variable $bdd non définie

--- assets/includes/db.php
+++ assets/includes/db.php
@@ -7,10 +7,11 @@ $username = 'root';          // à adapter selon ton serveur
 $password = '';              // souvent vide en local (XAMPP, MAMP)
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
+
 /* echo "[Test db.php : connexion réussie à la base de données.]";
 */
